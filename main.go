60596e27a9992d55a7db2daa531e518d72fce09e
Audie Caceres <audie.caceres1999@gmail.com>
2025-04-22 23:42:55 -0400

    remove ./app executable add connect database to app config

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
+	repo "go_base/audiecaceres7/internal/database"
 	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
 	"github.com/subosito/gotenv"
 )
 
@@ -15,6 +19,7 @@ const (
 
 type AppConfig struct {
     port string
+    Repo repo.Queries
 }
 
 func main() {
@@ -22,8 +27,24 @@ func main() {
     if err != nil {
         log.Printf("ERROR: couldn't load env, %v", err) 
     }
+    
+    ctx := context.Background()
+    conn, err := pgx.Connect(ctx, os.Getenv("DB_CONN_STRING"))
+    if err != nil {
+        log.Printf("ERROR: couldn't connect to database %v", err) 
+    }
+
+    appCofig := AppConfig{
+        port: PORT,
+        Repo: *repo.New(conn),
+    }
 
     router := gin.Default()
+    router.Use(func(c *gin.Context) {
+        c.Set("config", appCofig) 
+        c.Next()
+    })
+
     router.LoadHTMLGlob(TEMPLATES_PATH)
     router.Static("/static", STATIC_PATH)
 
